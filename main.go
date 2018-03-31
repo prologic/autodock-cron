4ca69340337c8a157e6fc929b68cb08192d14d82
James Mills <prologic@shortcircuit.net.au>
2018-03-31 10:24:42 -0700

    Basic working cron plugin that reschedules created containers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
 	"os/signal"
 	"syscall"
@@ -8,8 +9,14 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/docker/docker/api/types/events"
+	"github.com/robfig/cron"
+
 	"github.com/prologic/autodock/plugin"
 )
+
+// Key ...
+const Key = "autodock.cron.schedule"
 
 // CronPlugin ...
 var CronPlugin = &plugin.Plugin{
@@ -19,8 +26,37 @@ var CronPlugin = &plugin.Plugin{
 	for contaienr and service startup events and reschedules those
 	contaienrs and services according to their configured schedule.`,
 	Run: func(ctx plugin.Context) error {
+		c := cron.New()
+		c.Start()
+
 		ctx.On("container", func(id uint64, payload []byte, created time.Time) error {
-			log.Infof("id=%v payload=%s created=%v", id, payload, created)
+			var evt *events.Message
+			err := json.Unmarshal(payload, &evt)
+			if err != nil {
+				log.Errorf("error decoding payload: %s", err)
+				return err
+			}
+
+			cid := evt.ID
+
+			switch evt.Action {
+			case "create":
+				log.Infof("container creaed: %#v", evt)
+				err, val := ctx.GetLabel(cid, Key)
+				if err != nil {
+					log.Errorf("error getting container label: %s", err)
+				} else if val == "" {
+					log.Warnf(
+						"ignoring container %s with no valid label",
+						cid[len(cid)-10:],
+					)
+				} else {
+					c.AddFunc(val, func() {
+						ctx.StartContainer(cid)
+					})
+				}
+			}
+
 			return nil
 		})
 
